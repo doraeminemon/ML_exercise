48822a48993c02c27d456b53045a44b0937fb10b
Đỗ Đình Thy Sơn <tyson@pickupp.io>
2020-02-09 23:07:44 +0700

    Done week 6 / ex5

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -8,16 +8,17 @@ function [X_poly] = polyFeatures(X, p)
 
 % You need to return the following variables correctly.
 X_poly = zeros(numel(X), p);
-
+fprintf('Shape 1 %d %d \n', size(X_poly), size(X_poly, 2))
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
 %               column of X contains the values of X to the p-th power.
 %
 % 
-
-
-
-
+for j = 1:numel(X)
+  for i = 1:p
+    X_poly(j, i) = X(j) .^ i
+  end
+end
 
 
 % =========================================================================
